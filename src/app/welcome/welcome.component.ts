e8c510af9ec12b173b77c7a569b516e807265342
Maximilian Kern <maxkernw@gmail.com>
2017-03-18 12:43:14 +0100

    new welcome

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { slideToRight } from '../router.animations';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
+  animations: [slideToRight()],
+  host: {'[@routerTransition]': ''}
 })
 export class WelcomeComponent implements OnInit {
 
